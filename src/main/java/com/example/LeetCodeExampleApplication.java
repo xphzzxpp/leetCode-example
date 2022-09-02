57e5375f1a72f8d73d7cf335ef41845328e9a3be
mengwei <mengwei01@missfresh.cn>
2022-09-02 10:27:15 +0800

    dev

---
+++ src/main/java/com/example/LeetCodeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LeetCodeExampleApplication.class, args);
+  }
+
+}
